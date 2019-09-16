511b9392c532365340b32e1edd7f6bb64843973a
Danilo Andres Cortes <dacortes63@misena.edu.co>
2019-09-16 08:30:14 -0500

    chore: se configura el motor de plantillas handlebars

--- src/index.js
+++ src/index.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const exphbs = require('express-handlebars');
+const path = require('path');
 
 //Inicializaciones
 const app = express();
 
 //Configuraciones
 app.set('puerto', process.env.PORT || 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('.hbs', exphbs({
+    defaultLayout: 'main',
+    layoutsDir: path.join(app.get('views'), 'layouts'),
+    partialsDir: path.join(app.get('veiws'), 'partials'),
+    extname: '.hbs',
+    helpers: require('./lib/handlebars')
+}));
+app.set('view engine', '.hbs');
 
 
 //Middlewares
