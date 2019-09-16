944c891c664a54f7c77fd5302f0ee87d3c17bd20
Danilo Andres Cortes <dacortes63@misena.edu.co>
2019-09-16 12:37:46 -0500

    chore: se terminan las configuraciones iniciales para el proyecto con las rutas de autenticacion y enlaces

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayout: 'main',
     layoutsDir: path.join(app.get('views'), 'layouts'),
-    partialsDir: path.join(app.get('veiws'), 'partials'),
+    partialsDir: path.join(app.get('views'), 'partials'),
     extname: '.hbs',
     helpers: require('./lib/handlebars')
 }));
@@ -21,14 +21,23 @@ app.set('view engine', '.hbs');
 
 //Middlewares
 app.use(morgan('dev'));
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
 //Variables Gobales
 
+app.use((req, res, next) => {
+
+    next();
+});
+
 //Rutas
-app.use(require('./routes'))
+app.use(require('./routes'));
+app.use(require('./routes/autenticacion'));
+app.use('/enlaces', require( './routes/enlaces'));
 
 //Public
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Iniciando Servidor
 app.listen(app.get('puerto'), () => {
