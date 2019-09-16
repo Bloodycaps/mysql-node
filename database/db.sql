4e13af0101cf2f4cf08489fa32b851c776285551
Danilo Andres Cortes <dacortes63@misena.edu.co>
2019-09-16 16:52:38 -0500

    chore: se realiza el diseño de la base de datos

---
+++ database/db.sql
@@ -0,0 +1,23 @@
+CREATE DATABASE enlaces;
+
+USE enlaces;
+
+CREATE TABLE usuario(
+    id INT(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    nombreusuario VARCHAR(16) NOT NULL,
+    contraseña VARCHAR(60) NOT NULL,
+    nombrecompleto VARCHAR(100) NOT NULL
+);
+ DESCRIBE usuario;
+
+ CREATE TABLE enlaces (
+     id INT (11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+     titulo VARCHAR(150) NOT NULL,
+     url VARCHAR(255) NOT NULL,
+     descripcion TEXT,
+     user_id INT (11),
+     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+     CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES usuario(id)
+ );
+
+DESCRIBE enlaces;
