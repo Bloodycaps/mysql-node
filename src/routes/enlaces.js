6ce5887318dad8f0b45f83e897d2ea4b0a3f16bd
Danilo Andres Cortes <dacortes63@misena.edu.co>
2019-09-17 14:11:57 -0500

    chore: se realiza la configuracion de la vista para los nuevos links y el enrutamiento con los metodos get y post de los mismos

--- src/routes/enlaces.js
+++ src/routes/enlaces.js
@@ -4,4 +4,19 @@ const router = express.Router();
 const pool = require('../database');
 
 
+router.get('/anadir', (req, res) => {
+    res.render('enlaces/anadir');
+});
+
+router.post('/anadir', async (req, res) =>{
+    const { titulo, url, descripcion } = req.body;
+    const nuevoEnlace = {
+        titulo,
+        url,
+        descripcion
+    };
+    await pool.query(' INSERT INTO enlaces set ?', [nuevoEnlace]);
+    res.send('recibido')
+});
+
 module.exports = router;
