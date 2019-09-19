bb3fa16e193eb3109a778311d26a0e4bc3c03f67
Danilo Andres Cortes <dacortes63@misena.edu.co>
2019-09-19 10:30:33 -0500

    chore: se modifica las rutas para eliminar los enlaces de la vista general

--- src/routes/enlaces.js
+++ src/routes/enlaces.js
@@ -1,28 +1,32 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const pool = require('../database');
+const pool = require("../database");
 
-
-router.get('/anadir', (req, res) => {
-    res.render('enlaces/anadir');
+router.get("/anadir", (req, res) => {
+  res.render("enlaces/anadir");
 });
 
-router.post('/anadir', async (req, res) =>{
-    const { titulo, url, descripcion } = req.body;
-    const nuevoEnlace = {
-        titulo,
-        url,
-        descripcion
-    };
-    await pool.query(' INSERT INTO enlaces set ?', [nuevoEnlace]);
-    res.send('recibido')
+router.post("/anadir", async (req, res) => {
+  const { titulo, url, descripcion } = req.body;
+  const nuevoEnlace = {
+    titulo,
+    url,
+    descripcion
+  };
+  await pool.query(" INSERT INTO enlaces set ?", [nuevoEnlace]);
+  res.redirect("/enlaces");
 });
 
-router.get('/', async (req, res) =>{
-    const enlaces = await pool.query('SELECT * FROM enlaces');
-    res.render('enlaces/lista', { enlaces }); 
+router.get("/", async (req, res) => {
+  const enlaces = await pool.query("SELECT * FROM enlaces");
+  res.render("enlaces/lista", { enlaces });
 });
 
+router.get("/eliminar/:id", async (req, res) => {
+    const { id } = req.params;
+    await pool.query('DELETE FROM enlaces WHERE id = ?', [id]);
+    res.redirect('/enlaces');
+});
 
 module.exports = router;
